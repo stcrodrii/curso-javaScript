a2bcf676533cdf8ae3bc3ae1774f883b01ac7202
stcrodrii <cabralrodrigo793@gmail.com>
2022-02-14 10:35:20 -0300

    arrays basicos

---
+++ javaScript/arrays y arreglos/arrays basicos/arrays.js
@@ -0,0 +1,23 @@
+'use strict'    
+
+// arrays y areglos { es una coleccion de algo }
+
+
+// declaracion de la FORMA 1 
+var frutas = ["pera","coco","anana", "naranja"];
+console.log(frutas); 
+
+
+
+// declaracion de la FORMA 2 
+var comidad = [];
+comidad [0] = "asado "; 
+comidad [1] = "pizza "; 
+comidad [2] = "mila "; 
+comidad [3] = "pollo al horno "; 
+comidad [4] = "arroz con pollo"; 
+comidad [5] = 45;
+comidad [6] = true;
+
+console.log(comidad); // forma de ver todo el arrays
+console.log(comidad[5]); // forma de ver el arrays por posicion
