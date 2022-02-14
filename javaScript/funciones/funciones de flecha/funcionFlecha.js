3a4a37675c396469eed5c720b75f02333d9f3380
stcrodrii <cabralrodrigo793@gmail.com>
2022-02-14 08:46:31 -0300

    funcion de flecha

---
+++ javaScript/funciones/funciones de flecha/funcionFlecha.js
@@ -0,0 +1,13 @@
+'use strict'
+
+// funcion de flecha = es como definir una funcion normal pero con una fleta 
+
+
+function nombre (){
+    console.log(nombre);
+}
+
+
+nombre2  => {   // lo unico que sepone es el paremtro y la flecha que indica la funcion 
+    console.log(nombre2);
+}
