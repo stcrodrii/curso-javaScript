b7bdef534b406ba2de85b3d1b694578b5299e287
stcrodrii <cabralrodrigo793@gmail.com>
2022-02-16 09:35:43 -0300

    ordenar arrays alfebeticamente

---
+++ javaScript/arrays y arreglos/ordenar arrays/ordenar-arrays.js
@@ -0,0 +1,22 @@
+'use strict'
+
+// ordenar arrys alfabeticamente  
+//   .sort();
+
+var animales = ["gato","ballena" , "foca","araña", "dinosaurio","caballo","elefante"];
+console.log(animales);
+animales.sort(); // ordano alfabeticamnete
+console.log(animales);
+
+
+
+var numeros =[2,5,7,34,345,324,4,2,34,45,45,56,67,354,65,56,76,67,6734,54,2345,56,345,56,3,56,3456,56,356,356,3456,34];
+console.log(numeros);
+numeros.sort();
+console.log(numeros);  // ordena pero mal los numeros 
+
+
+// metodo para darle la vuelta al arrays 
+// .reverse();
+animales.reverse();
+console.log(animales); // se dio la vuelta
