7103834652aa5681c14a80cab0b6903feffb22fa
stcrodrii <cabralrodrigo793@gmail.com>
2022-02-16 11:44:54 -0300

    buscar elementos y comprar elementos dentro de un arrys

---
+++ javaScript/arrays y arreglos/buscar en un arrys/buscar-en-un-arrys.js
@@ -0,0 +1,39 @@
+'use strict'
+
+// buscar si el elementos de un arrys existe
+
+var lenguajes = ["java", "jacascript " , "php","css","html"]; 
+
+
+// forma 1 
+//  .find( function(arrys) return  arrys == "la busqueda"); 
+var busqueda = lenguajes.find(function(lenguajes){
+    return lenguajes == "java";
+});
+
+console.log(busqueda); /* si lo encuntra te muestra el elemento en la consola
+si no lo encunetra te arroja un undefine 
+*/  
+
+
+
+
+// forma 2
+var busqueda2 = lenguajes.find (lenguajes  =>  lenguajes == "css"); 
+console.log(busqueda2);
+
+
+
+
+// BUSCAR EL INDICE DE UN ELEMENTO y si existe 
+
+var busqueda3 = lenguajes.findIndex(lenguajes => lenguajes == "html" ); 
+console.log(busqueda3); // me muestra en la posicion en la q se encientra el elemento
+
+// FILTRO PARA SABER SI UN ELEMNTO ES MAYOR MENOR O IGUAL 
+// .some(arrys => arrys > 5 ) para saber si algun elemento es mayor a 5
+
+var num1 = [2,5,4,6,3,7,1,8,45,]; 
+
+var filtro = num1.some(num1 => num1 < 1);  // esto da false xq no existe elemento dentro del arrys menor a 1
+console.log(filtro); 
