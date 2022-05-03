72502d50951cdb3c7975f59514b821352654157e
stcrodrii <cabralrodrigo793@gmail.com>
2022-05-03 16:15:57 -0300

    mostrar los numeros entre 1-100 con un for

--- javaScript/bucle for/for.js
+++ javaScript/bucle for/for.js
@@ -1,10 +1,19 @@
 'use strict'
-
+// msotar los numero que existen entre el 1 y el 100
 // bucle FOR 
 
 
 for (var i = 1 ; i <= 100 ; i++){
+    if (i%2 ==0){
+        document.write(" ES PAR");
+
+    }
+    else {
+        document.write("es impar");
+    }
     console.log("numero = " +i );
+    document.write(" iteracion [" + i +"]"+ "<br>"); // <br> is a line break (salta una linea)
+    
 }
 
 
