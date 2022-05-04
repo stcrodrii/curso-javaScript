1160855ac42b25e1bbb74a9709ac423913412730
stcrodrii <cabralrodrigo793@gmail.com>
2022-05-04 16:02:35 -0300

    alertas y ventanas

--- javaScript/alertas y ventanas/alertas_incresoDedatos.js
+++ javaScript/alertas y ventanas/alertas_incresoDedatos.js
@@ -6,12 +6,14 @@ var n = "esto es otra alerta utilizando una variable";
 alert(n);
 
 
+
 // alertas de confirmacion 
 // se usa con la funcion {confirm();}
 confirm("¿quieres continuar?"); 
 
 var resultado = confirm("si o no ");
 console.log("esto es el resultado de la alerta de confirmacioon 2 ["+resultado+"]");
+document.write("esto es el resultado de la alerta de confirmacioon 2 ["+resultado+"]");
 
 
 // ingreso de datos 
@@ -21,5 +23,5 @@ var result = prompt("que edad tienes",0); // el 0 es el numero que nos pide por 
 console.log("resultado de la edad ["+ result+"]");
 
 // el resultado del prompt simepre es un tipo de dato string
-
+document.write("tu edad es = [" + result + "]");
 console.log(result+1); // para comprobar que es un string
