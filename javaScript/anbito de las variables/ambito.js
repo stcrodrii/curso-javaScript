e2268666acbe35e84054a5d9e2608e2207deb0bc
stcrodrii <cabralrodrigo793@gmail.com>
2022-05-12 16:22:24 -0300

    varaibles locales y globales

--- javaScript/anbito de las variables/ambito.js
+++ javaScript/anbito de las variables/ambito.js
@@ -2,7 +2,7 @@
 
 // ambito de las variables locales y globales
 
-
+var texto = "holasou un variable global ";  // esto es una variable global
 
 function varlocal (texto){
 
@@ -15,9 +15,10 @@ function varlocal (texto){
 
 }
 
-var texto = "holasou un variable global ";  // esto es una variable global 
 
 varlocal (texto);
+document.write(texto);
+document.write(texto2);// texto2 me tira un error xq es una variable local y no global ..
 
 
 //si yo llamo a la variable texto2 con un console.log me va tirar error poruqe solo funciona en el ambito de la funcion
