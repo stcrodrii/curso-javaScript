3424ac012ae358cee683728a389032016333a816
stcrodrii <cabralrodrigo793@gmail.com>
2022-05-12 16:41:04 -0300

    funcion lenght

---
+++ lenght/lenght.js
@@ -0,0 +1,8 @@
+'use strict'
+
+var numeros = [4,5,6,2,3,4,2,32,234,234,234,23,4,234,234,];
+
+for (var i=0 ; i<numeros.length ; i++ ){
+    document.write("posiccion "+ i + " = " +numeros[i] + "<br>") ; 
+}
+
