99b95c6a1113508de76611988194f21f08e5b5c9
stcrodrii <cabralrodrigo793@gmail.com>
2022-05-13 16:10:52 -0300

    arreglos

--- javaScript/arrays y arreglos/recorrer arreglos/recorrFor-ForEach.js
+++ javaScript/arrays y arreglos/recorrer arreglos/recorrFor-ForEach.js
@@ -5,13 +5,27 @@
 
 
 // completar un arrays con un for 
-var frutas = [10];
+var numero_de_frutas_que_usare = parseInt(prompt("ingrese cuantas frutas comprara" ));
+numero_de_frutas_que_usare = (numero_de_frutas_que_usare - 1 );
+var frutas = [];
 
-for (var i = 0 ; i < 10 ; i++){
+for (var i = 0 ; i <= numero_de_frutas_que_usare ; i++){
 
-frutas [i] = (prompt("ingresar la fruta numero "+ i));
+frutas [i] = (prompt("ingresar la fruta en la posicion  "+ i + " del arrys"));
 
 }
+
+//plantilla 
+var plantilla = `<h1> el numero de frutas que va a comprar es ${numero_de_frutas_que_usare + 1} <h1/> <br>
+<h3>Las frutas que seleciono son <h3/ >
+`;
+document.write(plantilla);
+
+// recorrer ese arrys y mostrar
+for (var j = 0 ; j < frutas.length ; j++){  
+    document.write("<li>"+ frutas[j] + "</li>");
+}
+
 
 
 //recorrer con for y mostar por la pag
@@ -32,10 +46,12 @@ frutas [i] = (prompt("ingresar la fruta numero "+ i));
 
 
 
-// for in 
+/* for in 
 
 for (let indice in frutas){  // creo una varaible local donde se guardara el indice y el arrays
 
     document.write(" <li>"+ frutas[indice] + "<li/>  " );  // muestro el arrays y entre corchetes le paso sus indices [ inidce]
 
 }
+
+*/
